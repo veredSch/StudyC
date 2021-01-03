1c099fb738a57bed767fceab1d2edb119e9dd5ad
estyyaish <Estyyaish@cs.colman.ac.il>
2021-01-03 11:08:56 +0200

    esty

--- StudyC/StudyC/Migrations/20210103064408_Init.Designer.cs
+++ StudyC/StudyC/Migrations/20210103064408_Init.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudyC.Data;
 
 namespace StudyC.Migrations
 {
     [DbContext(typeof(StudyCContext))]
-    partial class StudyCContextModelSnapshot : ModelSnapshot
+    [Migration("20210103064408_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
